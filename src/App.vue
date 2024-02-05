c8718b5bfd3d8ce5a167d66d74c48a041bf2e8a1
Yuto Teranishi <teranishi@rehabforjapan.com>
2024-02-05 20:46:26 +0900

    Calcアプリ作成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Calc :title="message" v-on:result-event="appAction"/>
+    <div class = "mt-3 text-left">
+      <table class="table" v-html="log"></table>
+    </div>
+    <div>
+      <button class="btn btn-danger" v-on:click="doClear">Clear log</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calc from './components/Calc.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calc
+  },
+  data() {
+    return {
+      message: 'CALC',
+      result: [],
+    }
+  },
+  computed: {
+    log() {
+      let table = `<tr><th>Expression</th><th>Value</th></tr>`
+      if (this.result.length > 0){
+        for(var i in this.result) {
+          table += '<tr><td>' + this.result[i][0] + '</td><th>' + this.result[i][0] + '</th></tr>'
+        }
+      }
+      return table
+    }
+  },
+  created() {
+    let items = localStorage.getItem('log')
+    let logs = JSON.parse(items)
+    if(logs != null) {
+      this.result = logs
+    }
+  },
+  methods: {
+    appAction(exp, res){
+      this.result.unshift([exp, res])
+      var log = JSON.stringify(this.result)
+      localStorage.setItem('log', log)
+    },
+    doClear() {
+      if(confirm('ログを全て消します.')) {
+        localStorage.removeItem('log')
+        this.result = []
+      }
+    }
   }
 }
 </script>
